71b8698e1ced19c057eb6fbce23adced08c6439a
Steve Gilland <steven.gilland@gmail.com>
2020-03-17 17:09:20 -0500

    Initial Activity Added.

---
+++ app/src/main/java/com/foundry/compleat/InventoryBuilder.kt
@@ -0,0 +1,12 @@
+package com.foundry.compleat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InventoryBuilder : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inventory_builder)
+    }
+}
